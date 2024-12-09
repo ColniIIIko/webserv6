75718e0cc2cf3240bed51aafb5054ac5f7a7dafe
Ivan Savickiy <isavitskiy12@gmail.com>
2024-12-09 14:22:39 +0300

    ref: move to postgres

--- db/db.go
+++ db/db.go
@@ -1,10 +1,9 @@
 package db
 
 import (
-	"fmt"
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -21,21 +20,10 @@ var db *gorm.DB
 func Init() {
 	var err error
 	var (
-		host     = os.Getenv("DB_HOST")
-		port     = os.Getenv("DB_PORT")
-		user     = os.Getenv("DB_USER")
-		password = os.Getenv("DB_PASSWORD")
-		dbname   = os.Getenv("DB_NAME")
+		connectionString = os.Getenv("DB_CONN")
 	)
 
-	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		user,
-		password,
-		host,
-		port,
-		dbname,
-	)
-	db, err = gorm.Open(mysql.Open(dns), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to the database")
 	}
